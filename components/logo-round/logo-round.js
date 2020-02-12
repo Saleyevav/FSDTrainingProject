621ac77e79858e40902952823c2044906794fcf1
Alexandr Saleev <saleyevav@yandex.ru>
2020-02-12 13:52:04 +0300

    Add logo-round element on the headers-and-footers page

---
+++ components/logo-round/logo-round.js
@@ -0,0 +1,1 @@
+import './logo-round.scss';
---
+++ pages/headers-and-footers/headers-and-footers.js
@@ -0,0 +1,4 @@
+import './../../theme/fonts.scss';
+import './../../theme/global.scss';
+import './../../theme/variables.scss';
+import './../../components/logo-round/logo-round.js';
